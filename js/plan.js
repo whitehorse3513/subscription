d4516c1309d95ba6fa4ec3a7e2e4a8267bf52ad6
whitehorse3513 <48043357+whitehorse3513@users.noreply.github.com>
2021-01-14 21:10:56 +0100

    update

--- js/plan.js
+++ js/plan.js
@@ -1,76 +1,28 @@
 $(document).ready(function(){
-
-    var current_fs, next_fs, previous_fs; //fieldsets
-    var opacity;
-    var current = 1;
-    var steps = $("fieldset").length;
-    
-    setProgressBar(current);
-    
-    $(".next").click(function(){
-    
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
-    
-    //Add Class Active
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-    
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
-    });
-    next_fs.css({'opacity': opacity});
-    },
-    duration: 500
-    });
-    setProgressBar(++current);
-    });
-    
-    $(".previous").click(function(){
-    
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
-    
-    //Remove class active
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-    
-    //show the previous fieldset
-    previous_fs.show();
-    
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
-    });
-    previous_fs.css({'opacity': opacity});
-    },
-    duration: 500
-    });
-    setProgressBar(--current);
-    });
-    
-    function setProgressBar(curStep){
-    var percent = parseFloat(100 / steps) * curStep;
-    percent = percent.toFixed();
-    $(".progress-bar")
-    .css("width",percent+"%")
-    }
-    
-    $(".submit").click(function(){
-    return false;
+    $('.card').on("click", function() {
+        if (!$(this).hasClass("active")) {
+            $('.card').removeClass('active');
+            $(this).addClass('active');
+            let value = $(this).data('value');
+            $('.selected-plan').text(value);
+        }
     })
-    
-    });
+
+    $('.people-number button').on("click", function() {
+        if (!$(this).hasClass("active")) {
+            $('.people-number button').removeClass('active');
+            $(this).addClass('active');
+            let value = $(this).data('value');
+            $('.selected-people').text(value);
+        }
+    })
+
+    $('.drink-number button').on("click", function() {
+        if (!$(this).hasClass("active")) {
+            $('.drink-number button').removeClass('active');
+            $(this).addClass('active');
+            let value = $(this).data('value');
+            $('.selected-drink').text(value);
+        }
+    })
+});
