1a1c4eea87eb1ea3ddc131396ea65226ffb3d0ab
whitehorse3513 <48043357+whitehorse3513@users.noreply.github.com>
2021-01-14 21:24:47 +0100

    update first page

--- js/plan.js
+++ js/plan.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
             $(this).addClass('active');
             let value = $(this).data('value');
             $('.selected-plan').text(value);
+            changedState();
         }
     })
 
@@ -14,6 +15,7 @@ $(document).ready(function(){
             $(this).addClass('active');
             let value = $(this).data('value');
             $('.selected-people').text(value);
+            changedState();
         }
     })
 
@@ -23,6 +25,72 @@ $(document).ready(function(){
             $(this).addClass('active');
             let value = $(this).data('value');
             $('.selected-drink').text(value);
+            changedState();
         }
     })
+    let changedState = () => {
+        let plan = $('.card.active').data('id');
+        let people = $('.people-number button.active').data('value');
+        
+        let serving = '';
+        switch (plan) {
+            case 1:
+                switch (people) {
+                    case 1:
+                        serving = '1,90'
+                        break;
+                    case 2:
+                        serving = '1,85'
+                        break;
+                    case 3:
+                        serving = '1,75'
+                        break;
+                }
+                break;
+            case 2:
+                switch (people) {
+                    case 1:
+                        serving = '2,90'
+                        break;
+                    case 2:
+                        serving = '2,85'
+                        break;
+                    case 3:
+                        serving = '2,75'
+                        break;
+                }
+                break;
+            case 3:
+                switch (people) {
+                    case 1:
+                        serving = '3,90'
+                        break;
+                    case 2:
+                        serving = '3,85'
+                        break;
+                    case 3:
+                        serving = '3,75'
+                        break;
+                }
+                break;
+            case 4:
+                switch (people) {
+                    case 1:
+                        serving = '4,29'
+                        break;
+                    case 2:
+                        serving = '4,19'
+                        break;
+                    case 3:
+                        serving = '4,00'
+                        break;
+                }
+                break;
+        }
+
+        $(".serving-price").text(serving);
+    }
+
+    changedState();
 });
+
